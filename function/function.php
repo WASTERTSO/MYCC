7246c4071de65c04111b4bf5b6339a6c37b77831
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2024-09-27 12:01:55 +0530

    Update function.php

--- function/function.php
+++ function/function.php
@@ -1,11 +1,11 @@
 <?php
 
 /*==========> INFO 
- * CODE     : BY ZLAXTERT
+ * CODE     : BY TSO SHIV
  * SCRIPT   : CC CHECKER
  * VERSION  : 4
- * TELEGRAM : t.me/zlaxtert
- * BY       : DARKXCODE
+ * TELEGRAM : t.me/imnotshiv
+ * BY       : TEAM_TSO
  */
 
  date_default_timezone_set("Asia/Jakarta");
